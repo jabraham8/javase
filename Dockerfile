5568fa38bccedfe061f3c5c899f3c11f257239c7
jarodriguez <jarodriguez@paradigmatecnologico.com>
2016-03-10 11:43:17 +0100

    imagen javase

---
+++ Dockerfile
@@ -0,0 +1,57 @@
+FROM centos:7
+MAINTAINER BOL Paradigma carrefour-bol@paradigmadigital.com
+
+#ADD local.repo /etc/yum.repos.d/
+ADD application.jar /
+
+ENV JAVA_VERSION 1.8.0
+ENV GID 20000
+ENV UID 20000
+
+ENV APP_HOME /opt/app 
+ENV IMAGE_SCRIPTS_HOME /opt/produban
+
+RUN mkdir -p $APP_HOME && \
+	mkdir $IMAGE_SCRIPTS_HOME && \
+    mkdir -p $IMAGE_SCRIPTS_HOME/certs && \
+    mkdir -p $IMAGE_SCRIPTS_HOME/bin
+
+COPY Dockerfile $IMAGE_SCRIPTS_HOME/Dockerfile
+COPY java-buildpack-memory-calculator $IMAGE_SCRIPTS_HOME/bin/java-buildpack-memory-calculator
+
+RUN groupadd --gid $GID java && useradd --uid $UID -m -g java java && \
+    yum -y install \
+       java-$JAVA_VERSION-openjdk-devel \
+       procps-ng \
+       strace \
+       unzip \
+       wget && \
+    yum clean all
+
+ADD scripts $IMAGE_SCRIPTS_HOME 
+ADD certs $IMAGE_SCRIPTS_HOME/certs
+
+RUN chown -R java:java $APP_HOME && \
+    chown -R java:java $IMAGE_SCRIPTS_HOME
+
+RUN cd $IMAGE_SCRIPTS_HOME/certs; ./importCerts.sh
+EXPOSE 8080
+#######################################################################
+##### We have to expose image metada as label and ENV 
+#######################################################################
+LABEL com.produban.imageowner="Products and Services" \
+      com.produban.description="Java 8 runtime for Spring boot microservices" \
+      com.produban.components="java8"
+
+ENV com.produban.imageowner="Products and Services" \
+    com.produban.description="Java 8 runtime for Spring boot microservices" \
+    com.produban.components="java8"
+
+USER java
+WORKDIR $IMAGE_SCRIPTS_HOME
+
+RUN export PATH=$PATH:$IMAGE_SCRIPTS_HOME
+
+ENTRYPOINT [ "./control.sh" ]
+CMD [ "start" ]
+
---
+++ scripts/start.d/0.start_javase.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+echo "========================================="
+echo "Starting application"
+echo "========================================="
+env | sort
+
+
+##We are going to precalculate default java heap configuration:
+
+#### When a container has its memory limited, it is reflected in /sys/fs/cgroup/memory/memory.limit_in_bytes
+##AVAIL_MEM=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+##let AVAIL_MEM_MB=$AVAIL_MEM/1024/1024
+##mem_literal="$AVAIL_MEM_MB"m
+
+#### default Java Heap configuration
+## Tool extracted from https://github.com/cloudfoundry/java-buildpack-memory-calculator
+# ./java-buildpack-memory-calculator -memoryWeights=heap:70,metaspace:15,native:10 -totMemory=512m
+##JAVA_HEAP=$(/opt/produban/bin/java-buildpack-memory-calculator -memoryWeights=heap:70,metaspace:15,native:10 -totMemory=$mem_literal)
+
+
+#### I have to validate if ARTIFACT_URL is available
+
+if [ -n "$ARTIFACT_URL" ]
+then
+  file=`basename "$ARTIFACT_URL"`
+  wget -q --no-check-certificate --connect-timeout=5 --read-timeout=10 --tries=2 -O "/tmp/$file" "$ARTIFACT_URL"
+
+  #### I have to unpackage the file to $APP_HOME
+
+  if [ $? -eq 0 ]
+  then
+
+    if [[ $ARTIFACT_URL = *.tgz* ]]
+    then
+       tar -xzvf /tmp/$file -C "$APP_HOME"
+    fi
+    if [[ $ARTIFACT_URL = *.zip* ]]
+    then
+       unzip  /tmp/$file  -d "$APP_HOME"
+    fi
+    if [[ $ARTIFACT_URL = *.jar* ]]
+    then
+       cp /tmp/$file "$APP_HOME"
+    fi
+	if [[ $ARTIFACT_URL = *.war* ]]
+    then
+       cp /tmp/$file "$APP_HOME"
+    fi
+  else
+    echo "ERROR: while Downloading file from $ARTIFACT_URL"
+    return 1
+  fi
+fi
+
+#### If JAR_PATH is empty I have to find a JAR in $APP_HOME
+
+if [ -z "$JAR_PATH" ]
+then
+   JAR_PATH=`find $APP_HOME -name "*.jar" -o -name "*.war" | head -1`
+fi
+
+if [ -z "$JAR_PATH" ]
+then
+   JAR_PATH="/application.jar"
+fi
+
+#### if you want to add extra JVM options,, you have to use the env variable JAVA_EXT_OPTS
+
+###echo java $JAVA_HEAP $JAVA_OPTS_EXT -jar "$JAR_PATH" $JAVA_PARAMETERS
+exec java $JAVA_HEAP $JAVA_OPTS_EXT -jar "$JAR_PATH" $JAVA_PARAMETERS
